789eb95a4f8bd4772219bdbe9a7496515c6f214b
leila111111 <leila_merzak@outlook.fr>
2023-07-23 02:45:18 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,23 +8,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp, *temp1;
-	current = (*list)->next;
-	while (current)
+	listint_t *temp, *curr_node, *temp1;
+
+	if (*list == NULL || (*list)->next == NULL || list == NULL)
+		return;
+	temp = (*list)->next;
+	curr_node = (*list)->next;
+	while (temp)
 	{
-		temp = current;
-		while (temp->prev != NULL && ((temp->n) < (temp->prev->n)))
+		temp = temp->next;
+		while (curr_node->prev != NULL && ((curr_node->n) < (curr_node->prev->n)))
 		{
-			temp1 = temp->next;
-			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
-			temp->next->next = temp1;
-			temp->next->prev = temp;
-			temp = temp->prev;
-			if (temp->prev == NULL)
-				*list = temp;
+			temp1 = curr_node->next;
+			curr_node->next = curr_node->prev;
+			curr_node->prev = curr_node->next->prev;
+			curr_node->next->next = temp1;
+			curr_node->next->prev = curr_node;
+			if (curr_node->next->next != NULL)
+				curr_node->next->next->prev = curr_node->next;
+			if (curr_node->prev == NULL)
+				*list = curr_node;
+			else
+				curr_node->prev->next = curr_node;
 			print_list(*list);
 		}
-		current = current->next;
+		curr_node = temp;
 	}
 }
